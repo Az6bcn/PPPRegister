a464ecb4ac78850e8df7ab2419714b0c9fd8371e
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2020-06-08 22:39:35 +0100

    route config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { CheckInComponent } from './check-in/check-in/check-in.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CheckedinListComponent } from './list/checkedin-list/checkedin-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CheckedinListComponent },
+  {path: 'check-in', component: CheckInComponent },
+  {path: 'checkedin-members', component: CheckedinListComponent},
+  {path: '**', component: CheckedinListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
